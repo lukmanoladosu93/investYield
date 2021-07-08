49f77b7a348b76d94ab4d4919ff678ad2791cb61
Ikenna Agu <kenelight@gmail.com>
2021-07-08 08:35:46 +0100

    Savings scrollbar and enhancements

--- investyield_signed_in/assets/js/custom.js
+++ investyield_signed_in/assets/js/custom.js
@@ -1,25 +1,68 @@
-$(window).scroll(function() {
-    var scroll = $(window).scrollTop();
-    if (scroll > 0) {
-        $(".header-desktop").addClass("header-desktop-active");
-    } else {
-        $(".header-desktop").removeClass("header-desktop-active");
-    }
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > 0) {
+    $(".header-desktop").addClass("header-desktop-active");
+  } else {
+    $(".header-desktop").removeClass("header-desktop-active");
+  }
 });
 
+$(".scrollbar-external").scrollbar({
+  autoScrollSize: false,
+  scrollx: $(".external-scroll_x"),
+  scrolly: $(".external-scroll_y"),
+});
 
-(function($) {
-    // 'use strict';
-    // weCare Slider
-    var wecareSwiper = new Swiper('.wecare-slider', {
-        slidesPerView: 1,
-        spaceBetween: 2,
-        loop: true,
-        centeredSlides: true,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        }
-    });
+$(".scrollbar-rail").scrollbar();
 
+//Scrollers with button
+console.log("init-scroll: " + $(".right-paddle").scrollLeft());
+$(".right-paddle").on("click", function () {
+  $(".menu-wrapper").animate(
+    {
+      scrollLeft: "+=100",
+    },
+    200
+  );
+});
+$(".left-paddle").on("click", function () {
+  $(".menu-wrapper").animate(
+    {
+      scrollLeft: "-=100",
+    },
+    200
+  );
+});
+
+//Scrollers with rails
+$(".right-paddle").on("click", function () {
+  $(".scroll-content").animate(
+    {
+      scrollLeft: "+=100",
+    },
+    200
+  );
+});
+$(".left-paddle").on("click", function () {
+  $(".scroll-content").animate(
+    {
+      scrollLeft: "-=100",
+    },
+    200
+  );
+});
+
+(function ($) {
+  // 'use strict';
+  // weCare Slider
+  var wecareSwiper = new Swiper(".wecare-slider", {
+    slidesPerView: 1,
+    spaceBetween: 2,
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 })(jQuery);
